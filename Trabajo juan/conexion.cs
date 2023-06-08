d079ca3783db8103ff7ebfb313497bb9be05f632
JuanIgimenez <90787861+JuanIgimenez@users.noreply.github.com>
2023-06-08 17:24:34 -0300

    conexion

---
+++ Trabajo juan/conexion.cs
@@ -0,0 +1,18 @@
+using MySql.Data.MySqlClient;
+
+string connectionString = "server=localhost;user=root;password=;database=horarios;";
+using (MySqlConnection connection = new MySqlConnection(connectionString))
+{
+    connection.Open();
+    string query = "SELECT * FROM tabla";
+    using (MySqlCommand command = new MySqlCommand(query, connection))
+    {
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+            // Procesar los datos leídos
+            }
+        }
+    }
+}
